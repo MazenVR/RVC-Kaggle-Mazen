0700481cd93c1b3b69c9cb14aaec3cae0781ebad
Mazen VR <fxegypt@hotmail.com>
2023-07-30 20:00:37 +0300

    Color neutral

--- infer-web.py
+++ infer-web.py
@@ -1618,7 +1618,7 @@ def get_presets():
     
     return preset_names
 
-with gr.Blocks(theme=gr.themes.Soft(primary_hue="slate").set(
+with gr.Blocks(theme=gr.themes.Soft(primary_hue="neutral").set(
     button_primary_background_fill="*primary_100",
     button_primary_background_fill_hover="*primary_150")) as app:
     gr.HTML("<h1> RVC [Fork by Mazen VR] إستنساخ الصوت بالذكاء الإصطناعي </h1>")
