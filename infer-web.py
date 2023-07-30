5a16d5e3c1a7822cf0f3961159b74767649aa8b5
Mazen VR <fxegypt@hotmail.com>
2023-07-30 19:45:08 +0300

    Fix Color

--- infer-web.py
+++ infer-web.py
@@ -1618,7 +1618,7 @@ def get_presets():
     
     return preset_names
 
-with gr.Blocks(theme=gr.themes.Default(secondary_hue="green")) as app:
+with gr.Blocks(theme=gr.themes.Soft()) as app:
     gr.HTML("<h1> RVC [Fork by Mazen VR] إستنساخ الصوت بالذكاء الإصطناعي </h1>")
     gr.Markdown(
         value="<b>https://www.youtube.com/@MohamedElmazen</b>"
